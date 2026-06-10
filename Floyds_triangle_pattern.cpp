7ff0c199744faad1f1a6e2a216ba1fb3230e469b
charu-tyagi <tyagicharu2007@gmail.com>
2026-06-10 18:25:01 +0530

    added floyd's triangle number and char versions.

---
+++ Floyds_triangle_pattern.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// TO PRINT FLOYD'S TRIANGLE PATTERN (NUMBER VERSION)
+
+   int n,num=1;
+   cout << "Enter nymber of lines : ";
+   cin >> n;
+   for(int i=0;i<n;i++){
+    for(int j=1;j<=(i+1);j++){
+        cout << num << " ";
+        num++;
+    }
+    cout << endl;
+   }
+
+// TO PRINT FLOYD'S TRIANGLE PATTERN (CHARACTER VERSION)
+
+    int N;
+    char ch='A';
+    cout << "Enter number of lines : ";
+    cin >> N;
+    for(int i=0;i<N;i++){
+        for(int j=1;j<=(i+1);j++){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+  
+    return 0;
+}
