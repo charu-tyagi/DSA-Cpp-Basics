3db853e4db029811711e0e430710ee681abc36f4
charu-tyagi <tyagicharu2007@gmail.com>
2026-06-12 13:58:20 +0530

    Added continuous square pattern

--- continuous_square_pattern.cpp
+++ continuous_square_pattern.cpp
@@ -5,25 +5,23 @@ int main(){
 
 // TO PRINT NUMBERS CONTINUOUSLY IN "N" NUMBER OF LINES
 
-    int i,j,n,num=1;
+    int n,num=1;
     cout << "Enter number of lines : ";
     cin >> n;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             cout << num << " ";
             num++;
         }
         cout << endl;
     }
 
-// TO PRINT CHARACTERS CONTINUOUSLY IN "R" NUMBER OF LINES
+// TO PRINT CHARACTERS CONTINUOUSLY IN "N" NUMBER OF LINES
        
-    int p,q,r;
     char ch = 'A';
-    cout << "Enter number of lines : ";
-    cin >> r;
-    for(p=0;p<r;p++){
-        for(q=0;q<r;q++){
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             cout << ch << " ";
             ch++;
         }
